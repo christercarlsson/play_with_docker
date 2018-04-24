849791e7a77fb7f1c93578cf66bb262abfb92099
Carlsson Christer <ccarlsson@outlook.com>
2018-04-24 13:56:34 +0200

    Some more stuff

--- src/Services/RandomUserRepository.cs
+++ src/Services/RandomUserRepository.cs
@@ -8,21 +8,23 @@ namespace tinywebapp.Services {
     public class RandomUserRepository : IContactRepository
     {
         readonly HttpClient _httpClient;
+        private const string api = "/api/?exc=login";
 
-        public RandomUserRepository()
+        public RandomUserRepository(HttpClient httpClient)
         {
-            _httpClient = new HttpClient();
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://randomuser.me");
         }
 
         public async Task<Contact> GetContact(Id id)
         {
-            var result = await _httpClient.GetStringAsync("https://randomuser.me/api/");            
+            var result = await _httpClient.GetStringAsync(api);            
             return Contact.FromJson(result).Results[0];
         }
 
         public async Task<IEnumerable<Contact>> GetContacts()
         {
-            var result = await _httpClient.GetStringAsync("https://randomuser.me/api/");
+            var result = await _httpClient.GetStringAsync(api);
            
             var contacts = Contact.FromJson(result);
             var tmp = new List<Contact>(contacts.Results);
